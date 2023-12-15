b754ad440be33f1faedb288e631e27869aa22261
MOZZ\admin <yeong0319@naver.com>
2023-12-15 17:30:40 +0900

    init : firstcategory엔티티 추가, typeorm설치

---
+++ src/first-categories/first-categories.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class FirstCategories {
+  @PrimaryGeneratedColumn()
+  firstCategoryId: number;
+
+  @Column()
+  firstCategoryName: string;
+}
