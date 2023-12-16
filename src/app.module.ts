644959bc4736bb74835148ba3bc89202b4ffeab2
MOZZ\admin <yeong0319@naver.com>
2023-12-16 16:56:36 +0900

    init : 카테고리 관리를 위해서 manager인증이 필요할거같아서 manager기본 파일 구조 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirstCategoriesModule } from './first-categories/first-categories.module';
+import { ManagersModule } from './managers/managers.module';
 
 @Module({
-  imports: [FirstCategoriesModule],
+  imports: [FirstCategoriesModule, ManagersModule],
   controllers: [AppController],
   providers: [AppService],
 })
