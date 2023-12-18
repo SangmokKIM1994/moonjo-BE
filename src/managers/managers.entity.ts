2ee394907e44a6f3f9268766f73f59fdacb5a882
MOZZ\admin <yeong0319@naver.com>
2023-12-18 09:21:16 +0900

    feat : 비밀번호 암호화 추가

--- src/managers/managers.entity.ts
+++ src/managers/managers.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @Entity()
 export class Managers {
@@ -13,4 +15,16 @@ export class Managers {
 
   @Column()
   password: string;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try {
+      const hashKey = process.env.HASHKEY;
+      this.password = await bcrypt.hash(this.password, Number(hashKey));
+    } catch (error) {
+      throw new InternalServerErrorException(
+        '비밀번호 암호화에 문제가 생겼습니다.',
+      );
+    }
+  }
 }
