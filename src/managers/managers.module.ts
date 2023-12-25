0763ada2cdf4bba5eb014043ce26da5d4d57b614
MOZZ\admin <yeong0319@naver.com>
2023-12-25 10:01:52 +0900

    fix : Managers를 사용하기위해 의존성 주입

--- src/managers/managers.module.ts
+++ src/managers/managers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ManagersController } from "./managers.controller";
 import { ManagersService } from "./managers.service";
+import { Managers } from "./managers.entity";
 
 @Module({
+  imports: [Managers],
   controllers: [ManagersController],
   providers: [ManagersService],
 })
