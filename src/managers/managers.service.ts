f4e3c4e8524cdec3876ab890703114794b1bad69
MOZZ\admin <yeong0319@naver.com>
2023-12-27 16:02:35 +0900

    fix : controller에서 service를 service에서 repository를 사용할수있게 인스턴스생성

--- src/managers/managers.service.ts
+++ src/managers/managers.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Managers } from "./managers.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class ManagersService {}
+export class ManagersService {
+  constructor(
+    @InjectRepository(Managers)
+    private readonly managerRepository: Repository<Managers>
+  ) {}
+}
