f75c8976a3f39cf69414ed8d2c3b5f3b1d56ae47
MOZZ\admin <yeong0319@naver.com>
2023-12-29 10:12:01 +0900

    fix : manager추가 api를 만들기 위해 dto 생성, dto에 필요한 class-validator추가

--- src/managers/managers.service.ts
+++ src/managers/managers.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Managers } from "./managers.entity";
 import { Repository } from "typeorm";
@@ -9,4 +9,12 @@ export class ManagersService {
     @InjectRepository(Managers)
     private readonly managerRepository: Repository<Managers>
   ) {}
+
+  async createManager() {
+    try {
+      
+    } catch (error) {
+      throw new InternalServerErrorException("매니저 추가 시 서버 에러");
+    }
+  }
 }
