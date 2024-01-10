e536daab9ed249ba5116087fcde4c7ec5ef87d55
MOZZ\admin <yeong0319@naver.com>
2024-01-10 19:13:38 +0900

    fix : controller에서 service, service에서 DB접근가능한 인스턴스 생성

--- src/first-categories/first-categories.controller.ts
+++ src/first-categories/first-categories.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from "@nestjs/common";
 
 @Controller("first-categories")
-export class FirstCategoriesController {}
+export class FirstCategoriesController {
+  constructor(
+    private readonly firstCategoriesController: FirstCategoriesController
+  ) {}
+}
--- src/first-categories/first-categories.service.ts
+++ src/first-categories/first-categories.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FirstCategories } from "./first-categories.entity";
+import { Managers } from "src/managers/managers.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class FirstCategoriesService {}
+export class FirstCategoriesService {
+  constructor(
+    @InjectRepository(FirstCategories)
+    private readonly firstCategoriesRepository: Repository<FirstCategories>,
+    @InjectRepository(Managers)
+    private readonly managerRepository: Repository<Managers>
+  ) {}
+}
