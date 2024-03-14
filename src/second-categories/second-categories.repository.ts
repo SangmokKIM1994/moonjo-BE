5454d3ba9fd9f137de99d028c44e3efae6adff45
MOZZ\admin <yeong0319@naver.com>
2024-03-14 20:34:50 +0900

    init : second-category에 repository추가

---
+++ src/second-categories/second-categories.repository.ts
@@ -0,0 +1,7 @@
+import { Managers } from "src/managers/managers.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+export class SecondCategoriesRepository {
+  constructor() {}
+}
