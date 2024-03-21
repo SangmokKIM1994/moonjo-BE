3ef9e7cba7e78a0029c58c931e4f2e4441e30712
MOZZ\admin <yeong0319@naver.com>
2024-03-21 20:50:03 +0900

    init : firstcategory에 의존성 주입

--- src/first-categories/first-categories.module.ts
+++ src/first-categories/first-categories.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
 import { FirstCategoriesController } from "./first-categories.controller";
 import { FirstCategoriesService } from "./first-categories.service";
+import { FirstCategoriesRepository } from "./first-categories.repository";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { FirstCategories } from "./first-categories.entity";
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([FirstCategories]),
+    FirstCategoriesRepository,
+  ],
   controllers: [FirstCategoriesController],
   providers: [FirstCategoriesService],
 })
