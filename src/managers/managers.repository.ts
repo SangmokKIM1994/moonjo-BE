4919fd3cfb0472f5577b0f75740d88c42e713220
MOZZ\admin <yeong0319@naver.com>
2024-03-30 00:58:45 +0900

    fix : manager의 create를 repository와 연결

--- src/managers/managers.repository.ts
+++ src/managers/managers.repository.ts
@@ -3,9 +3,23 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Managers } from "./managers.entity";
 import { Repository } from "typeorm";
 
-export class managerRepository {
+export class ManagerRepository {
   constructor(
     @InjectRepository(Managers)
     private readonly managersRepository: Repository<Managers>
   ) {}
+
+  async createManager(CreateManagerDto) {
+    const { name, id, password, email } = CreateManagerDto;
+    const result = this.managersRepository.create({
+      name,
+      id,
+      password,
+      email,
+    });
+
+    await this.managersRepository.save(result);
+    
+    return result;
+  }
 }
